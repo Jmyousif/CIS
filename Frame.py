8c86b884c0c9c5ca3f37490bad04e916265bcd38
Jonah <Jmyousif@gmail.com>
2018-10-23 15:43:16 -0400

    new

--- Frame.py
+++ Frame.py
@@ -1,13 +1,15 @@
 import math
 import numpy as np
+import PointTransformations as PT
 
-class Frame():
-    def __init__(self, rot = np.zeros(3), tr = 0):
+
+class Frame:
+    def __init__(self, rot=np.zeros(3, 3), tr=np.zeros(3, 1)):
         try:
             if rot.shape[0] != 3 or rot.shape[1] != 3:
                 raise ValueError
             self.rot = rot
-            if tr.shape[0] != 1 or tr.shape[1] != 3:
+            if tr.shape[0] != 3 or tr.shape[1] != 1:
                 raise ValueError
             self.tr = tr
         except ValueError:
@@ -23,23 +25,32 @@ class Frame():
 
     def setTr(self, tr):
         try:
-            if tr.shape[0] != 1 or tr.shape[1] != 3:
+            if tr.shape[0] != 3 or tr.shape[1] != 31:
                 raise ValueError
             self.tr = tr
         except ValueError:
             print("input vector is not a column vector!")
 
-    def FFmult(self, F):
+    def invert(self, f):
         try:
-            if not isinstance(F, Frame):
+            if not isinstance(f, Frame):
                 raise ValueError
-            return Frame(self.rot * F.rot, self.rot * F.tr + self.tr)
+            f.setRot(np.invert(f.rot))
+            f.setTr(np.dot(-f.rot, f.tr))
+        except ValueError:
+            print("Not a frame!")
+
+    def FFmult(self, f):
+        try:
+            if not isinstance(f, Frame):
+                raise ValueError
+            return Frame(self.rot * f.rot, np.dot(self.rot, f.tr) + self.tr)
         except ValueError:
             print("not a frame!")
 
     def FPmult(self, p):
         try:
-            if p.shape[0] != 1 or p.shape[1] != 3:
+            if p.shape[0] != 3 or p.shape[1] != 1:
                 raise ValueError
             return self.rot * p + self.tr
         except ValueError:
