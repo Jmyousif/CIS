48d1b54b5fe37ae9c1c6620c31ff4fed0d8f0b8d
Jonah <Jmyousif@gmail.com>
2018-10-25 21:44:12 -0400

    MORE TESTS

--- unit_tests.py
+++ unit_tests.py
@@ -10,60 +10,63 @@ import em_tracking
 import opti_tracker
 import distortion_calibration
 
-class unit_testing():
-    #testing Constructor
+
+class unit_testing:
+    # Testing Constructor
     testMatrix1 = np.zeros((3, 3))
     testVector1 = np.zeros((3, ))
-    print(testVector1.ndim)
     testFrame1 = Frame.Frame(testMatrix1, testVector1)
     assert np.array_equal(testFrame1.getRot(), np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
     assert np.array_equal(testFrame1.getTr(), np.array([0, 0, 0]))
 
-    #Testing setRot
+    # Testing setRot
     testFrame1.setRot(np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]]))
-    assert (testFrame1.getRot() == np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])).all()
+    assert np.array_equal(testFrame1.getRot(), np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]]))
 
-    #Testing setTr
+    # Testing setTr
     testFrame1.setTr(np.array([1, 2, 3]))
-    assert (testFrame1.getTr() == np.array([1, 2, 3])).all()
+    assert np.array_equal(testFrame1.getTr(), np.array([1, 2, 3]))
 
-    #Testing Invert
+    # Testing Invert
     testFrameInv = testFrame1.invert()
-    assert (testFrameInv.getRot() == np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]])).all()
+    assert np.array_equal(testFrameInv.getRot(), np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]]))
 
-    #Testing FFmult
+    # Testing FFmult
     testMatrix2 = np.array([[0, 0, 1], [0, 1, 0], [-1, 0, 0]])
     testVector2 = np.array([3, 2, 1])
     testFrame2 = Frame.Frame(testMatrix2, testVector2)
     testFFprod = testFrame1.FFmult(testFrame2)
-    assert (testFFprod.getRot() == np.array([[0, -1, 0], [0, 0, 1], [-1, 0, 0]])).all()
-    assert (testFFprod.getTr() == np.array([-1, 5, 4])).all()
-    assert (testFFprod.getTr() == (np.array([-1, 5, 4]))).all()
+    assert np.array_equal(testFFprod.getRot(), np.array([[0, -1, 0], [0, 0, 1], [-1, 0, 0]]))
+    assert np.array_equal(testFFprod.getTr(), np.array([-1, 5, 4]))
+    assert np.array_equal(testFFprod.getTr(), (np.array([-1, 5, 4])))
 
-    #Testing FPmult
+    # Testing FPmult
     testVector3 = np.array([7, 9, 5])
     testFPprod = testFrame1.FPmult(testVector3)
-    assert (testFPprod == np.array([-8, 9, 8])).any()
+    assert np.array_equal(testFPprod, np.array([-8, 9, 8]))
 
-    #Testing pivot calibration
+    # Testing pivot calibration
+    # TestMatrix2 = [[0, 0, 1], [0, 1, 0], [-1, 0, 0]], testVector3 = [7, 9, 5]
     p_list = pivot_calibration.pivot_calibration(testMatrix2, testVector3, 10)
-    print(p_list)
+    print(p_list[1])
+    array = [3.5, 4.5, 2.5]
+    assert np.array_equal(p_list[1], np.array([3.5, 4.5, 2.5]).reshape(3,0))
+    # The unknown p_t in this problem should be [3.5, 4.5, 2.5].
 
-    #Testing Three-Dimensional Transform, rotation with [[0, -1, 0], [1, 0, 0], [0, 0, 1]] and then translation with
-        # [3, 2, 1]
-    
+    # Testing Three-Dimensional Transform, rotation with [[0, -1, 0], [1, 0, 0], [0, 0, 1]] and then translation with
+    # [3, 2, 1]
     pointset1 = np.array([[0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7], [6, 7, 8], [7, 8, 9],
                           [8, 9, 10], [9, 10, 11]])
     pointset2 = np.array([[2, 2, 3], [1, 3, 4], [0, 4, 5], [-1, 5, 6], [-2, 6, 7], [-3, 7, 8], [-4, 8, 9],
                           [-5, 9, 10], [-6, 10, 11], [-7, 11, 12]])
     threedframe = three_dimension_transform.rigid_transform(pointset1, pointset2)
     print(threedframe.getRot(), threedframe.getTr())
-    # 90 degree rotation, add 1,2,3
+    # 90 degree rotation matrix, add 3,2,1
 
-    #Testing EM_Tracker
+    # Testing EM_Tracker
     #print(em_tracking.EM_track(0))
 
-    #Testing Optical_Tracker
+    # Testing Optical_Tracker
     #print(opti_tracker.opti_track(0))
 
     # #To test with read-in files
